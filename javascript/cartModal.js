d506ceb97f54fa7be9050c369b6a995a8e5effab
Chabbe <henriksson.charlie@gmail.com>
2019-12-10 11:21:46 +0100

    Added modal

---
+++ javascript/cartModal.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+$("#basket").on("click", function(){
+            
+    $(this).siblings().each(function(i) {
+        if(i > 0) {
+            console.log($(this).prev().html());
+            let span = $("<span>").html($(this).prev().html());
+            $("#modalcontent").append(span);
+        }
+    });
+    //let p = $('<p>').html("<b>hello</b>");
+    
+    $("#divmodal, .innerModal").addClass("active");
+    
+});
+// Ta bort class "active"
+$(".close, #divmodal").on("click", function() {
+    $("#divmodal, .innerModal").removeClass("active");
+}); 
+
+});
